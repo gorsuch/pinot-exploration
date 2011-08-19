f8402683eb5b0cd79e7569bd5df195cd766e4164
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-08-19 11:48:51 -0500

    simple cljs optimizations to make debugging easier

--- src/pinot_exploration/server.clj
+++ src/pinot_exploration/server.clj
@@ -10,5 +10,6 @@
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
-                        :ns 'pinot-exploration})))
+                        :ns 'pinot-exploration
+                        :cljsc {:optimizations :simple :pretty-print true}})))
 
